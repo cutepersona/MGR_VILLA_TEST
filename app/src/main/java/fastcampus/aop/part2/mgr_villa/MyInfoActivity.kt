9729ed2c3b3aab6f312cb1b1f7a678dd1f170b10
cutepersona <kogs01@naver.com>
2022-01-25 15:45:43 +0900

    MyInfoAdd commit v2 2022-01-25

--- app/src/main/java/fastcampus/aop/part2/mgr_villa/MyInfoActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/mgr_villa/MyInfoActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.bumptech.glide.Glide
+import de.hdodenhof.circleimageview.CircleImageView
 import fastcampus.aop.part2.mgr_villa.customdialog.LogOutDialog
+import fastcampus.aop.part2.mgr_villa.database.VillaNoticeHelper
 import fastcampus.aop.part2.mgr_villa.databinding.ActivityMyinfoBinding
+import fastcampus.aop.part2.mgr_villa.model.VillaNotice
+import fastcampus.aop.part2.mgr_villa.sharedPreferences.MyApplication
 import kotlinx.android.synthetic.main.activity_myinfo.*
 
 class MyInfoActivity : AppCompatActivity() {
@@ -17,10 +22,26 @@ class MyInfoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        initToolBar()
+        Glide.with(this).load(R.raw.chunsic).into(binding.MyInfoImage)
 
+        initToolBar()
+        initMyInfo()
         initHomeBottomNavigationBar()
 
+    }
+
+    // 내 기본정보 가져오기
+    private fun initMyInfo() {
+        val villaNoticedb = VillaNoticeHelper.getInstance(applicationContext)
+        Thread(Runnable {
+            val userInfo = villaNoticedb!!.VillaNoticeDao().getUser(
+                    MyApplication.prefs.getString("email","").trim()
+            )
+
+            runOnUiThread {
+                binding.InfoUserName.setText(userInfo.userName)
+            }
+        }).start()
     }
 
     override fun onBackPressed() {
