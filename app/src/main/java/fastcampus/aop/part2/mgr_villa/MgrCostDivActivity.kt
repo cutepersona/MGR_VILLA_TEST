f9e3ec8fecef3ecf48c37a865d08a43cfb748f53
cutepersona <kogs01@naver.com>
2022-02-10 13:53:58 +0900

    FireStore StandardCostUpdate commit v2 2022-02-10

--- app/src/main/java/fastcampus/aop/part2/mgr_villa/MgrCostDivActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/mgr_villa/MgrCostDivActivity.kt
@@ -60,35 +60,35 @@ class MgrCostDivActivity: AppCompatActivity() {
     private fun initButtonSetOnClick() {
         binding.MgrCostButton.setOnClickListener {
 
-            val villaNoticedb = VillaNoticeHelper.getInstance(applicationContext)
-
-            Thread(Runnable {
-                val ConstCost = villaNoticedb!!.VillaNoticeDao().isConstCost(
-                    MyApplication.prefs.getString("villaAddress","").trim()
-                )
-
-                val accountCheck = villaNoticedb!!.VillaNoticeDao().isAccount(
-                    MyApplication.prefs.getString("villaAddress","").trim()
-                )
-
-                val favoriteAccountCheck = villaNoticedb!!.VillaNoticeDao().isFavoriteAccount()
-
-                runOnUiThread {
-                    if (ConstCost <= 0) {
-                        showToast("기준관리비가 등록되어 있지 않습니다.")
-                    } else if (accountCheck <= 0) {
-                        showToast("계좌가 등록되어 있지 않습니다.")
-                    } else if (favoriteAccountCheck <= 0) {
-                        showToast("주 계좌가 선택되어 있지 않습니다.")
-                    } else {
-                        val TenantCostListActivity = Intent(this, TenantCostListActivity::class.java)
-                        startActivity(TenantCostListActivity)
-                    }
-
-                }
-            }).start()
-
-
+//---------------------------------------------------------------------------------------
+//            val villaNoticedb = VillaNoticeHelper.getInstance(applicationContext)
+//
+//            Thread(Runnable {
+//                val ConstCost = villaNoticedb!!.VillaNoticeDao().isConstCost(
+//                    MyApplication.prefs.getString("villaAddress","").trim()
+//                )
+//
+//                val accountCheck = villaNoticedb!!.VillaNoticeDao().isAccount(
+//                    MyApplication.prefs.getString("villaAddress","").trim()
+//                )
+//
+//                val favoriteAccountCheck = villaNoticedb!!.VillaNoticeDao().isFavoriteAccount()
+//
+//                runOnUiThread {
+//                    if (ConstCost <= 0) {
+//                        showToast("기준관리비가 등록되어 있지 않습니다.")
+//                    } else if (accountCheck <= 0) {
+//                        showToast("계좌가 등록되어 있지 않습니다.")
+//                    } else if (favoriteAccountCheck <= 0) {
+//                        showToast("주 계좌가 선택되어 있지 않습니다.")
+//                    } else {
+//                        val TenantCostListActivity = Intent(this, TenantCostListActivity::class.java)
+//                        startActivity(TenantCostListActivity)
+//                    }
+//
+//                }
+//            }).start()
+//---------------------------------------------------------------------------------------
 
         }
 
--- app/src/main/java/fastcampus/aop/part2/mgr_villa/MgrStandardCostActivity.kt
+++ app/src/main/java/fastcampus/aop/part2/mgr_villa/MgrStandardCostActivity.kt
@@ -1,15 +1,19 @@
 package fastcampus.aop.part2.mgr_villa
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextUtils
 import android.text.TextWatcher
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.isInvisible
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import fastcampus.aop.part2.mgr_villa.database.VillaNoticeHelper
 import fastcampus.aop.part2.mgr_villa.databinding.ActivityMgrstandardcostBinding
 import fastcampus.aop.part2.mgr_villa.model.StandardCost
@@ -21,6 +25,8 @@ import java.time.LocalDate
 class MgrStandardCostActivity: AppCompatActivity() {
 
     private val binding: ActivityMgrstandardcostBinding by lazy { ActivityMgrstandardcostBinding.inflate(layoutInflater) }
+
+    val firestoreDB = Firebase.firestore
 
     private var StandardCostFlag = false
     private var tonCostAmount: String = ""
@@ -38,7 +44,11 @@ class MgrStandardCostActivity: AppCompatActivity() {
         initSetStandardCost()
     }
 
-
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val MgrCostDivActivity = Intent(this, MgrCostDivActivity::class.java)
+        startActivity(MgrCostDivActivity)
+    }
 
 
     private fun initToolBar() {
@@ -51,43 +61,77 @@ class MgrStandardCostActivity: AppCompatActivity() {
 
     // 툴바 백버튼
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val id = item.itemId
-        when (id) {
-            android.R.id.home -> {
-                finish()
-                return true
-            }
-        }
 
-        return super.onOptionsItemSelected(item)
+        val MgrCostDivActivity = Intent(this, MgrCostDivActivity::class.java)
+        startActivity(MgrCostDivActivity)
+
+        return true
+
+//        val id = item.itemId
+//        when (id) {
+//            android.R.id.home -> {
+//                finish()
+//                return true
+//            }
+//        }
+//
+//        return super.onOptionsItemSelected(item)
     }
 
 
     // 관리비 기준금액 등록 후 확인
     private fun initSetStandardCost() {
-        val villadb = VillaNoticeHelper.getInstance(applicationContext)
-
-        Thread(Runnable {
-            val standardCost = villadb?.VillaNoticeDao()?.getStandardCost(
-                    MyApplication.prefs.getString("villaAddress","").trim()
-            )
-
-            runOnUiThread {
-                if (standardCost == null) {
-                    binding.tonCostEditText.setText("")
-                    binding.cleanCostEditText.setText("")
-                    binding.usunCostEditText.setText("")
-                    binding.mgrCostEditText.setText("")
-                } else {
-                    binding.tonCostEditText.setText(standardCost.tonCost.toString())
-                    binding.cleanCostEditText.setText(standardCost.cleanCost.toString())
-                    binding.usunCostEditText.setText(standardCost.usunCost.toString())
-                    binding.mgrCostEditText.setText(standardCost.mgrCost.toString())
+        firestoreDB.collection("StandardCost")
+            .whereEqualTo("villaAddr", MyApplication.prefs.getString("villaAddress", "").trim())
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firestoreDB.collection("StandardCost")
+                        .whereEqualTo("villaAddr", MyApplication.prefs.getString("villaAddress", "").trim())
+                        .get()
+                        .addOnSuccessListener { result ->
+                            if(result.isEmpty){
+                                binding.tonCostEditText.setText("")
+                                binding.cleanCostEditText.setText("")
+                                binding.usunCostEditText.setText("")
+                                binding.mgrCostEditText.setText("")
+                            }else {
+                                for (i in task.result!!) {
+                                    binding.tonCostEditText.setText(i.data["tonCost"].toString())
+                                    binding.cleanCostEditText.setText(i.data["cleanCost"].toString())
+                                    binding.usunCostEditText.setText(i.data["usunCost"].toString())
+                                    binding.mgrCostEditText.setText(i.data["mgrCost"].toString())
+                                    break
+                                }
+                            }
+                        }
                 }
-
             }
-        }).start()
 
+//--------------------------------------------------------------------------------
+//        val villadb = VillaNoticeHelper.getInstance(applicationContext)
+//
+//        Thread(Runnable {
+//            val standardCost = villadb?.VillaNoticeDao()?.getStandardCost(
+//                    MyApplication.prefs.getString("villaAddress","").trim()
+//            )
+//
+//            runOnUiThread {
+//                if (standardCost == null) {
+//                    binding.tonCostEditText.setText("")
+//                    binding.cleanCostEditText.setText("")
+//                    binding.usunCostEditText.setText("")
+//                    binding.mgrCostEditText.setText("")
+//                } else {
+//                    binding.tonCostEditText.setText(standardCost.tonCost.toString())
+//                    binding.cleanCostEditText.setText(standardCost.cleanCost.toString())
+//                    binding.usunCostEditText.setText(standardCost.usunCost.toString())
+//                    binding.mgrCostEditText.setText(standardCost.mgrCost.toString())
+//                }
+//
+//            }
+//        }).start()
+//--------------------------------------------------------------------------------
     }
 
     // 관리비 기준금액 등록 버튼
@@ -96,23 +140,87 @@ class MgrStandardCostActivity: AppCompatActivity() {
             if (!checkForm()) {
                 return@setOnClickListener
             } else {
-                val villaNoticedb = VillaNoticeHelper.getInstance(applicationContext)
-                Thread(Runnable {
-                    villaNoticedb!!.VillaNoticeDao().standardCostInsert(
-                        StandardCost(
-                            MyApplication.prefs.getString("villaAddress","").trim(),
-                            binding.tonCostEditText.text.toString().trim().replace(",","").toInt(),
-                            binding.cleanCostEditText.text.toString().trim().replace(",","").toInt(),
-                            binding.usunCostEditText.text.toString().trim().replace(",","").toInt(),
-                            binding.mgrCostEditText.text.toString().trim().replace(",","").toInt()
-                        )
-                    )
 
-                    runOnUiThread {
-                        val MgrCostDivActivity = Intent(this, MgrCostDivActivity::class.java)
-                        startActivity(MgrCostDivActivity)
+                // 기준관리비 체크
+                firestoreDB.collection("StandardCost")
+                    .whereEqualTo("villaAddr", MyApplication.prefs.getString("villaAddress", "").trim())
+                    .get()
+                    .addOnSuccessListener { result ->
+                        // 기존 기준관리비가 있을때 update
+                        if(!result.isEmpty){
+                            firestoreDB.collection("StandardCost")
+                                .whereEqualTo("villaAddr",MyApplication.prefs.getString("villaAddress",""))
+                                .get()
+                                .addOnSuccessListener { results ->
+                                    for(i in results){
+                                        firestoreDB.collection("StandardCost")
+                                            .document(i.id)
+                                            .update(mapOf(
+                                                "tonCost" to binding.tonCostEditText.text.toString().trim().replace(",","").toInt(),
+                                                "cleanCost" to binding.cleanCostEditText.text.toString().trim().replace(",","").toInt(),
+                                                "usunCost" to binding.usunCostEditText.text.toString().trim().replace(",","").toInt(),
+                                                "mgrCost" to binding.mgrCostEditText.text.toString().trim().replace(",","").toInt()
+                                            ))
+                                        break
+                                    }
+                                    val MgrCostDivActivity = Intent(this, MgrCostDivActivity::class.java)
+                                    startActivity(MgrCostDivActivity)
+                                }
+
+                        }else {
+                            // 기준관리비 정보가 없을때 신규 등록
+
+                            val standardCost = firestoreDB.collection("StandardCost")
+
+                            val StandardCost = hashMapOf(
+                                "villaAddr" to MyApplication.prefs.getString("villaAddress", "").trim(),
+                                "tonCost" to binding.tonCostEditText.text.toString().trim().replace(",","").toInt(),
+                                "cleanCost" to binding.cleanCostEditText.text.toString().trim().replace(",","").toInt(),
+                                "usunCost" to binding.usunCostEditText.text.toString().trim().replace(",","").toInt(),
+                                "mgrCost" to binding.mgrCostEditText.text.toString().trim().replace(",","").toInt()
+                            )
+
+                            standardCost.document()
+                                .set(StandardCost)
+                                .addOnSuccessListener { documentReference ->
+//                        Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                                    val MgrCostDivActivity = Intent(this, MgrCostDivActivity::class.java)
+                                    startActivity(MgrCostDivActivity)
+                                }
+                                .addOnFailureListener { e ->
+                                    showToast("기준 관리비 등록에 실패하였습니다.")
+                                    Log.w(ContentValues.TAG, "Error adding document", e)
+                                    return@addOnFailureListener
+                                }
+                        }
                     }
-                }).start()
+
+
+
+
+
+//------------------------------------------------------------------------------
+//                val villaNoticedb = VillaNoticeHelper.getInstance(applicationContext)
+//                Thread(Runnable {
+//                    villaNoticedb!!.VillaNoticeDao().standardCostInsert(
+//                        StandardCost(
+//                            MyApplication.prefs.getString("villaAddress","").trim(),
+//                            binding.tonCostEditText.text.toString().trim().replace(",","").toInt(),
+//                            binding.cleanCostEditText.text.toString().trim().replace(",","").toInt(),
+//                            binding.usunCostEditText.text.toString().trim().replace(",","").toInt(),
+//                            binding.mgrCostEditText.text.toString().trim().replace(",","").toInt()
+//                        )
+//                    )
+//
+//
+//                    runOnUiThread {
+//                        val MgrCostDivActivity = Intent(this, MgrCostDivActivity::class.java)
+//                        startActivity(MgrCostDivActivity)
+//                    }
+//                }).start()
+//------------------------------------------------------------------------------
+
+
             }
         }
     }
