d6f4444cbc500799bd1594f37dea90d1c63b9b70
cutepersona <kogs01@naver.com>
2022-02-16 13:38:17 +0900

    AddSplashActivity commit v3 2022-02-16

---
+++ app/src/main/java/fastcampus/aop/part2/mgr_villa/SplashActivity.kt
@@ -0,0 +1,40 @@
+package fastcampus.aop.part2.mgr_villa
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import fastcampus.aop.part2.mgr_villa.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private val binding : ActivitySplashBinding by lazy { ActivitySplashBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        },DURATION)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+    }
+    companion object {
+        private const val DURATION : Long = 3000
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+}
