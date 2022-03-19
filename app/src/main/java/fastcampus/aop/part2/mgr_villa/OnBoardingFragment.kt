31e707e5391ebe6e8b0d49b0f35064deebda4593
cutepersona <kogs01@naver.com>
2022-03-19 12:18:55 +0900

    gradle versionCodeUp 2022-03-19
    1. introindicator 추가

--- app/src/main/java/fastcampus/aop/part2/mgr_villa/OnBoardingFragment.kt
+++ app/src/main/java/fastcampus/aop/part2/mgr_villa/OnBoardingFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.viewpager2.widget.ViewPager2
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 import fastcampus.aop.part2.mgr_villa.adapter.IntroViewPagerAdapter
 import fastcampus.aop.part2.mgr_villa.databinding.FragmentOnBoardingBinding
+import kotlinx.android.synthetic.main.fragment_on_boarding.*
 
 class OnBoardingFragment : Fragment() {
 
@@ -24,6 +28,7 @@ class OnBoardingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         //1
         setupViewPager()
+
     }
 
     private fun setupViewPager() {
@@ -41,6 +46,7 @@ class OnBoardingFragment : Fragment() {
         )
 
         binding.introViewPager.adapter = adapter
+        binding.dotsIndicator.setViewPager2(binding.introViewPager)
         //2
         // 사용자가 버튼눌러서 넘어가게끔 함 이거는 추후 수정필요
 //        binding.introViewPager.isUserInputEnabled = false
---
+++ app/src/main/java/fastcampus/aop/part2/mgr_villa/viewholder/IntroViewHolder.kt
@@ -0,0 +1,15 @@
+package fastcampus.aop.part2.mgr_villa.viewholder
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import fastcampus.aop.part2.mgr_villa.R
+
+class IntroViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView){
+
+    val noticeNo: TextView = itemView.findViewById(R.id.noticeNo)
+
+
+
+
+}
